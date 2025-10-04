aa757c7aea967a681aebaf1dd133a95df08a8478
Mohamed Nasr <legendnasr@gmail.com>
2025-10-04 23:20:25 +0300

    4-oct-25

--- src/app/components/movies/movies.ts
+++ src/app/components/movies/movies.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MoviesService } from '../../shared/movies-service';
+import { Imovie } from '../../models/imovie';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './movies.html',
   styleUrl: './movies.css'
 })
 export class Movies {
+  movies$!: Observable<Imovie[]>;
+
+  imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+  constructor(private movieService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.movies$ = this.movieService.getMovies();
+  }
 
 }
--- src/app/shared/movies-service.ts
+++ src/app/shared/movies-service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Imovie } from '../models/imovie';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
-  
+  private baseURL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<Imovie[]> {
+    return this.http.get<{ results: Imovie[] }>(this.baseURL).pipe(map((res) => res.results));
+  }
+
+  getMovie(id: number): Observable<Imovie> {
+    return this.http.get<Imovie>(`https://api.themoviedb.org/3/movie/${id}?api_key=YOUR_API_KEY`);
+  }
+
+  // the following work only if using your own backend API
+  addMovie(movie: Imovie): Observable<Imovie> {
+    return this.http.post<Imovie>(this.baseURL, movie);
+  }
+
+  updateMovie(id: number, newMovie: Imovie): Observable<Imovie> {
+    return this.http.put<Imovie>(`${this.baseURL}/${id}`, newMovie);
+  }
+
+  deleteMovie(id: number): Observable<unknown> {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
 }
