a77b14ae32c98e236b21975d3c621a2161b7388b
Mohamed Nasr <legendnasr@gmail.com>
2025-10-05 01:06:33 +0300

    5-oct-2025

--- src/app/components/movies/movies.ts
+++ src/app/components/movies/movies.ts
@@ -15,8 +15,33 @@ import { HttpClient } from '@angular/common/http';
 export class Movies {
   movies$!: Observable<Imovie[]>;
 
+  currentPage = 1;
+  totalPages = 1;
+  pages: number[] = [];
+
   imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-  constructor(private movieService: MoviesService) {
+
+  constructor(private movieService: MoviesService) {}
+
+  ngOnInit(): void {
     this.movies$ = this.movieService.getMovies();
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.movieService.getMovies(page);
+    }
+  }
+
+  getRatingColor(vote: number): string {
+    const percent = vote * 10;
+    if (percent >= 70) {
+      return `conic-gradient(#21d07a ${percent * 3.6}deg, #204529 0deg)`;
+    } else if (percent >= 50) {
+      return `conic-gradient(#d2d531 ${percent * 3.6}deg, #423d0f 0deg)`;
+    } else {
+      return `conic-gradient(#db2360 ${percent * 3.6}deg, #571435 0deg)`;
+    }
   }
 }
--- src/app/shared/movies-service.ts
+++ src/app/shared/movies-service.ts
@@ -8,12 +8,16 @@ import { map, Observable } from 'rxjs';
 })
 export class MoviesService {
   private apiKey = 'cdf737fb0a8771a79228a4d738740585';
-  private baseURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${this.apiKey}`;
+  private baseURL = `https://api.themoviedb.org/3/movie/now_playing`;
 
   constructor(private http: HttpClient) {}
 
-  getMovies(): Observable<Imovie[]> {
-    return this.http.get<{ results: Imovie[] }>(this.baseURL).pipe(map((res) => res.results));
+  getMovies(page: number = 1): Observable<Imovie[]> {
+    return this.http
+      .get<{ results: Imovie[] }>(
+        `${this.baseURL}?api_key=${this.apiKey}&language=en-US&page=${page}`
+      )
+      .pipe(map((res) => res.results));
   }
 
   getMovie(id: number): Observable<Imovie> {
@@ -22,16 +26,15 @@ export class MoviesService {
     );
   }
 
-  // the following work only if using your own backend API
-  addMovie(movie: Imovie): Observable<Imovie> {
-    return this.http.post<Imovie>(this.baseURL, movie);
-  }
+  // addMovie(movie: Imovie): Observable<Imovie> {
+  //   return this.http.post<Imovie>(this.baseURL, movie);
+  // }
 
-  updateMovie(id: number, newMovie: Imovie): Observable<Imovie> {
-    return this.http.put<Imovie>(`${this.baseURL}/${id}`, newMovie);
-  }
+  // updateMovie(id: number, newMovie: Imovie): Observable<Imovie> {
+  //   return this.http.put<Imovie>(`${this.baseURL}/${id}`, newMovie);
+  // }
 
-  deleteMovie(id: number): Observable<unknown> {
-    return this.http.delete(`${this.baseURL}/${id}`);
-  }
+  // deleteMovie(id: number): Observable<unknown> {
+  //   return this.http.delete(`${this.baseURL}/${id}`);
+  // }
 }
